eec3334d9e3319cbf8aa55b93c95fc923eb9c03e
Manimaran-M3 <147019422+Manimaran-M3@users.noreply.github.com>
2025-01-18 17:58:29 +0530

    App.js

--- App.js
+++ App.js
@@ -3,8 +3,6 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './LoginScreen';
 import RegistrationScreen from './RegisterScreen';
-import HomeScreen from './HomeScreen';
-import DetailsScreen from './DetailsScreen';
 import ForgotPassword from './ForgotPassword';
 
 
@@ -17,8 +15,6 @@ export default function App() {
       <Stack.Navigator initialRouteName="Login">
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Register" component={RegistrationScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
         <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
       </Stack.Navigator>
     </NavigationContainer>
